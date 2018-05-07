e7c0084db09ba24241e9e5cb0a9356aa7f53e441
Nicholas Gustafson <nichgus7@gmail.com>
2018-05-06 22:50:51 -0400

    Added the description to the main slide show. The arrows are not bug proof yet, but they will be

--- scripts/homePage.js
+++ scripts/homePage.js
@@ -1,21 +1,144 @@
-var upperBar = document.getElementById("upper");
-var lowerBar = document.getElementById("lower");
-var titleText = document.getElementsByClassName("titleText");
+var introductionScript, quoteScript, whySoftwareScript;
+var upperBar = document.getElementById("upperBar");
+var lowerBar = document.getElementById("lowerBar");
+var upperQuote = document.getElementById("upperQuote");
+var lowerQuote = document.getElementById("lowerQuote");
+var titleText = document.getElementsByClassName("titleText")[0];
+var arrowWrapper = document.getElementsByClassName("arrowWrapper");
+var leftArrow = document.getElementById("leftArrow");
+var rightArrow = document.getElementById("rightArrow");
 var titleTextIsOrange = true;
+var i = 0, j = 0;
 
-titleText[0].classList.toggle("fadeInAnimaiton");
+fadeIn(titleText);
 
-setTimeout(function() { moveUpperBarDown(); }, 1600);
+setTimeout(function() {
+    moveUpperBarDown();
+    changeTitleTextTo(introductionScript[0]);
+}, 1600);
 
-setTimeout(function() { changeTitleTextTo("Why Software?"); }, 1600);
+setTimeout(function() {
+    moveBarsUp();
+    changeTitleTextTo(introductionScript[1]);
+}, 3200);
 
-setTimeout(function() { moveBarsUp(); }, 3200);
+setTimeout(function() {
+    moveBottomBarDown();
+    changeTitleTextTo(introductionScript[2]);
+}, 4800);
 
-setTimeout(function() { changeTitleTextTo("Well it's kind of a long story..."); }, 3200);
+setTimeout(function() {
+    upperBar.classList.toggle("widen");
+    upperBar.classList.toggle("moveToTop");
 
-setTimeout(function() { moveBottomBarDown(); }, 4800);
+    lowerBar.classList.toggle("widen");
+    lowerBar.classList.toggle("moveToBottom");
 
-setTimeout(function() { changeTitleTextTo("So here it is"); }, 4800);
+    fadeOut(titleText);
+    setTimeout(function() {
+        titleText.classList.toggle("titleText");
+        titleText.classList.toggle("whySoftwareExplination");
+        titleText.innerHTML = whySoftwareScript[j];
+        titleText.style.color = "black";
+        fadeIn(titleText);
+    }, 500);
+
+    setTimeout(function() {
+        upperQuote.innerHTML = quoteScript[i]
+        fadeIn(upperQuote);
+
+        lowerQuote.innerHTML = quoteScript[++i];
+        fadeIn(lowerQuote);
+
+        fadeIn(arrowWrapper[1]);
+        fadeIn(rightArrow);
+    }, 1000);
+}, 6400); // 6400
+
+function rightArrowClicked() {
+    if (i + 1 > quoteScript.length && j + 1 > whySoftwareScript.length) {
+        i = quoteScript.length - 2;
+        j = whySoftwareScript.length - 1;
+    }
+    setNewQuotes(++i % quoteScript.length, ++i % quoteScript.length);
+    changeTitleTextTo(whySoftwareScript[++j]);
+    if (j == 1) {
+        fadeIn(arrowWrapper[0]);
+        fadeIn(leftArrow);
+    } else if (j == quoteScript.length - 1) {
+        fadeOut(arrowWrapper[1]);
+        fadeOut(rightArrow);
+    }
+}
+
+function leftArrowClicked() {
+    if (i - 2 < 0 && j - 1 < 0) {
+        i = 2;
+        j = 1;
+    }
+    i -= 2;
+    setNewQuotes(i, ++i);
+    changeTitleTextTo(whySoftwareScript[--j]);
+    if (j == 0) {
+        fadeOut(leftArrow);
+        fadeOut(arrowWrapper[0]);
+    } else if (j == quoteScript.length - 2) {
+        fadeIn(rightArrow);
+        fadeIn(arrowWrapper[1]);
+    }
+}
+
+function setNewQuotes(firstIndex, secondIndex) {
+    fadeOut(upperQuote);
+    fadeOut(lowerQuote);
+
+    setTimeout(function() {
+        upperQuote.innerHTML = quoteScript[firstIndex]
+        fadeIn(upperQuote);
+
+        lowerQuote.innerHTML = quoteScript[secondIndex];
+        fadeIn(lowerQuote);
+    }, 1000);
+}
+
+function changeTitleTextTo(message) {
+    fadeOut(titleText);
+    setTimeout(function() {
+        titleText.innerHTML = message;
+        if (titleText.classList.contains("titleText")) {
+            switchTextColors();
+        }
+        fadeIn(titleText);
+    }, 500);
+}
+
+function fadeOut(element) {
+    if (element.classList.contains("fadeInAnimaiton")) {
+        element.classList.toggle("fadeInAnimaiton");
+        element.classList.toggle("fadeOutAnimaiton");
+    } else {
+        element.classList.toggle("fadeOutAnimaiton");
+    }
+}
+
+function fadeIn(element) {
+    if (element.classList.contains("fadeOutAnimaiton")) {
+        element.classList.toggle("fadeOutAnimaiton");
+        element.classList.toggle("fadeInAnimaiton");
+    } else {
+        element.classList.toggle("fadeInAnimaiton");
+    }
+}
+
+function switchTextColors() {
+    if (titleTextIsOrange) {
+        titleText.style.color = "#522D80";
+        titleTextIsOrange = false;
+    } else {
+        titleText.style.color = "#F66733";
+        titleTextIsOrange = true;
+    }
+}
 
 function moveUpperBarDown() {
     upperBar.classList.toggle("moveDown");
@@ -30,23 +153,14 @@ function moveBottomBarDown() {
     lowerBar.classList.toggle("moveUp");
 }
 
-function changeTitleTextTo(message) {
-    titleText[0].classList.toggle("fadeInAnimaiton");
-    titleText[0].classList.toggle("fadeOutAnimaiton");
-    setTimeout(function() {
-        titleText[0].innerHTML = message;
-        switchTextColors();
-        titleText[0].classList.toggle("fadeOutAnimaiton");
-        titleText[0].classList.toggle("fadeInAnimaiton");
-    }, 400);
+function sendIntroductionList(introductionScript) {
+    this.introductionScript = introductionScript;
 }
 
-function switchTextColors() {
-    if (titleTextIsOrange) {
-        titleText[0].style.color = "#522D80";
-        titleTextIsOrange = false;
-    } else {
-        titleText[0].style.color = "#F66733";
-        titleTextIsOrange = true;
-    }
+function sendQuoteList(quoteScript) {
+    this.quoteScript = quoteScript;
+}
+
+function sendWhySoftwareList(whySoftwareScript) {
+    this.whySoftwareScript = whySoftwareScript;
 }
---
+++ scripts/introductionText.js
@@ -0,0 +1,20 @@
+sendIntroductionList([
+    "Why Software?",
+    "Well it's kind of a long story...",
+    "So here it is"
+]);
+sendQuoteList([
+    "\"There\'s lots of ways to be, as a person.\" - Steve Jobs",
+    "\"The problem, often not discovered until late in life, is that when you look for things in life like love, meaning, motivation, it implies they are sitting behind a tree or under a rock. The most successful people in life recognize, that in life they create their own love, they manufacture their own meaning, they generate their own motivation.\" - Neil deGrasse Tyson",
+    "\"One of the ways I believe people express their appreciations to the rest of humanity is to make something wonderful and put it out there\" - Steve Jobs",
+    "\"I will do what I can when I can so that when I cannot I will not wish that I would have when I could have\" - Dabo Swinney",
+    "\"Do or do not, there is no try\" - Yoda"
+]);
+sendWhySoftwareList([
+    "This is how I express my deep appreciation. I believe that there is nothing more amazing in this world than a true smile, and seeing someone truly smile at my work or because of my work makes me reflect on how much I love what I do. So I chose to start there, figuring out how to make people smile.",
+    "Clearly, there is a connection between the outcome of working hard and making others happy. The activity that taught me how to work hard was playing the trumpet. It taught me that being apart of a group of people with the same interest can make a bigger impact. This is the first area that I got a lot of people to smile, as well as respect because they knew I worked hard to play the trumpet well.",
+    "Through many hours of practicing and many auditions, I was accepted into a handful of superior bands, which is where I met a true group of A-players which excited and amazed me. Everyone was passionate about what they were doing, which in turn made everyone who came to hear us smile.",
+    "So why did I transition over to a life of software development? Well, I see myself, among other things, to be a very logical and analytical thinker that is always thinking about how things work under the hood. I also believe there are far more passionate people in software development than any other field. Therefore, one could make truly amazing software and distribute it to the world.",
+    "While I know I am only highlighting the ending I must also say that life is not only about the finished product but also the journey. Along my journey, I have met some amazing people as well as had some unforgettable experiences, not only working on making a finished product better but also having fun with whoever worked with me outside of the product.",
+    "So I am continuing to do what I know best: work hard and have fun doing it. This is how I stay true to myself and how I remind myself what is truly important to me."
+]);
