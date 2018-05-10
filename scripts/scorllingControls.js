b422923860fa9747c5ed50c59f408d2fd6cc7da9
Nicholas Gustafson <nichgus7@gmail.com>
2018-05-09 20:06:16 -0400

    Made fix for the off spacing in the skills list

---
+++ scripts/scorllingControls.js
@@ -0,0 +1,63 @@
+$(window).on("load",function() {
+    $(window).scroll(function() {
+        var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+        $(".pageTitle").each(function() {
+            var titleBottom = $(this).offset().top + $(this).outerHeight();
+
+            if ($(this).css("opacity") == 0 && titleBottom < windowBottom) {
+                $(this).fadeTo(500,1);
+            }
+        });
+
+        $(".experience").each(function(i) {
+            var experienceContainer = $(this).offset().top + ($(this).outerHeight()/4);
+
+            if (experienceContainer < windowBottom) {
+                if ($(this).css("opacity") == 0) {
+                    $(this).fadeTo(500, 1);
+                }
+            }
+        });
+
+        $(".experienceDurationBar").each(function(i) {
+            var experienceContainer = $(this).offset().top + ($(".experienceDurationContainer").outerHeight()*.3);
+
+            if (experienceContainer < windowBottom) {
+                if ($(this).css("height") == "0px") {
+                    $(this).toggleClass("drawInAnimation");
+                    i *= 2;
+                    var date = document.getElementsByClassName("experienceDate");
+                    fadeIn(date[i]);
+                    setTimeout(function () {
+                        fadeIn(date[++i]);
+                    }, 1000);
+                }
+            }
+        });
+
+        $(".verticalTimelineBar").each(function() {
+            var experienceContainer = $(this).offset().top + ($(".experienceDurationContainer").outerHeight()*.2);
+
+            if (experienceContainer < windowBottom) {
+                if ($(this).css("height") == "0px") {
+                    $(this).toggleClass("drawInAnimationMainBar");
+                    setTimeout(function () {
+                        $('.dot').each(function(i) {
+                            $(".dot:nth-child(" + (++i) + ")").fadeTo(500, 1);
+                        });
+                    }, 2000);
+                }
+            }
+        });
+
+        $(".skillBar").each(function() {
+            var skillBar = $(this).offset().top + ($(this).outerHeight()*.2);
+
+            if (skillBar < windowBottom) {
+                if ($(this).css("height") == "0px") {
+                    $(this).toggleClass("drawInAnimationMainBar");
+                }
+            }
+        });
+    }).scroll();
+});
--- scripts/skills.js
+++ scripts/skills.js
@@ -12,27 +12,42 @@ function findRatingPath(rating) {
 }
 
 function makeSkill(item) {
-    return "<p class=\"skill\">" + item.name + " <img class=\"stars\" hspace=\"20\" src=\"" +
-    findRatingPath(item.stars) + "\"></p>";
+    return "<p class=\"skill\">" + item.name + "</p>";
+}
+
+function makeRating(item) {
+    return "<img class=\"stars\" hspace=\"20\" src=\"" + findRatingPath(item.stars) + "\">"
 }
 
 function sendSkillsLanguages(languageSkills) {
     var languageSkillsContainer = document.getElementById("languageSkills");
     fillSkills(languageSkillsContainer, languageSkills);
+    var languageRatingContainer = document.getElementById("languageRatings");
+    fillRatings(languageRatingContainer, languageSkills);
 }
 
 function sendSkillsFramewors(frameworkSkills) {
     var frameworkSkillsContainer = document.getElementById("frameworkSkills");
     fillSkills(frameworkSkillsContainer, frameworkSkills);
+    var frameworkRatingContainer = document.getElementById("frameworkRatings");
+    fillRatings(frameworkRatingContainer, frameworkSkills);
 }
 
 function sendSkillsTools(toolSkills) {
     var toolSkillsContainer = document.getElementById("toolSkills");
     fillSkills(toolSkillsContainer, toolSkills);
+    var toolSkillsContainer = document.getElementById("toolRatings");
+    fillRatings(toolSkillsContainer, toolSkills);
 }
 
-function fillSkills(container, list) {
-    for(var i = 0; i < list.length; i++) {
-        container.innerHTML += makeSkill(list[i]);
+function fillSkills(skillContainer, list) {
+    for (var i = 0; i < list.length; i++) {
+        skillContainer.innerHTML += makeSkill(list[i]);
+    }
+}
+
+function fillRatings(ratingContainer, list) {
+    for (var i = 0; i < list.length; i++) {
+        ratingContainer.innerHTML += makeRating(list[i]);
     }
 }
